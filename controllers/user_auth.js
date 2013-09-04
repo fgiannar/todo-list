aef4d728312047bd6028a4813a682727b6193e80
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2013-09-04 16:55:50 +0300

    fixes #53

--- controllers/user_auth.js
+++ controllers/user_auth.js
@@ -1,80 +1,48 @@
-(function() {
+/* jslint node: true */
+"use strict";
+
+
 var connection, mongo, express;
 mongo = require('mongodb');
 connection = require('../connection');
 
 
 exports.auth_user = function (req, res, next) {
-	 var header=req.headers['authorization'];        // get the header
-     if (!header)
-     	next("Authorization needed");
+    var header = req.headers['authorization']; // get the header
+    if (!header)
+        next("Authorization needed");
 
-     var token=header.split(/\s+/).pop()||'',            // and the encoded auth token
-      auth=new Buffer(token, 'base64').toString(),    // convert from base64
-      parts=auth.split(/:/),                          // split on colon
-      email=parts[0]||'',
-      pass=parts[1]||'';
+    var token = header.split(/\s+/).pop() || '', // and the encoded auth token
+        auth = new Buffer(token, 'base64').toString(), // convert from base64
+        parts = auth.split(/:/), // split on colon
+        email = parts[0] || '',
+        pass = parts[1] || '';
 
-    return connection(function(db) {
-		db.collection('users', function(errCollection, collection) {
-			collection.findOne({'email': email}, function(err, user) {
-				 
-				if (err) {
-					return next('An Error occured');
-			    }
-			    // no user then an account was not found for that email address
-				if (!user) {
-					console.info(user);
-					return next('Unknown email address ' + email);
-				}
-console.info(user);
-				// If we have a user lets compare the provided password with the
-     			// user's passwordHash
-				if (pass !== user.password) {
-					console.info('Invalid');
-					return next('Invalid Password');
-				}
-			
-				req.user = user;
-				
-				return next();
+    return connection(function (db) {
+        db.collection('users', function (errCollection, collection) {
+            collection.findOne({
+                'email': email
+            }, function (err, user) {
 
-			});
-		});
-    });
-    //next('a');
-   // next(validate(req, username, password));
-};
+                if (err) {
+                    return next('An Error occured');
+                }
+                // no user then an account was not found for that email address
+                if (!user) {
+                    return next('Unknown email address ' + email);
+                }
 
-var validate = function(req, email, pass) {
-	 // We need to look up the user by email address
- 	return connection(function(db) {
-		db.collection('users', function(errCollection, collection) {
-			collection.findOne({'email': email}, function(err, user) {
-				 
-				if (err) {
-					next('An Error occured');
-			    }
-			    // no user then an account was not found for that email address
-				if (!user) {
-					console.info(user);
-					next('Unknown email address ' + email);
-				}
-console.info(user);
-				// If we have a user lets compare the provided password with the
-     			// user's passwordHash
-				if (pass !== user.password) {
-					console.info('Invalid');
-					next('Invalid Password');
-				}
-			
-				req.user = user;
-				
-				next();
+                // If we have a user lets compare the provided password with the
+                // user's passwordHash
+                if (pass !== user.password) {
+                    return next('Invalid Password');
+                }
 
-			});
-		});
+                req.user = user;
+
+                return next();
+
+            });
+        });
     });
 };
-
-}).call(this);
